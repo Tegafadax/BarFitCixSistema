fbe94502e26b0dbd6fe6a7da8d534fbf3e93a8ee
Tegafadax <progtgfx@gmail.com>
2025-06-08 21:40:23 -0500

    Se Subio El Proyecto Base

---
+++ src/main/java/com/bfc/BarFitCixSistema/BarFitCixSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.bfc.BarFitCixSistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarFitCixSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarFitCixSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfc/BarFitCixSistema/BarFitCixSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfc.BarFitCixSistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarFitCixSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
